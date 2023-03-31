b2901f1db334a05d91da09271e352783e1779bec
Kyle Crawford <kcraw217@gmail.com>
2023-03-31 10:03:47 -0700

    complete business logic

--- js/script.js
+++ js/script.js
@@ -3,7 +3,15 @@
 function beepBoop(userInput) {
   let output = [];
   for (let i = 0; i <= userInput; i++) {
-    output.push(i);
+    if (i.toString().includes(3)){
+      output.push("Won't you be my neighbor?");
+    } else if (i.toString().includes(2)){
+      output.push("Boop");
+    } else if (i.toString().includes(1)){
+      output.push("Beep");
+    } else {
+      output.push(i);
+    }
   }
 
 
