9718764f26eae3bd84cee47de5532ff3fa4618b2
Kyle Crawford <kcraw217@gmail.com>
2023-03-31 10:12:29 -0700

    add utility logic

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
+// Utility Logic
+
+function includesNumber(input, number) {
+  const output = input.toString().includes(number);
+  return output;
+}
+
+
 // Business Logic
 
 function beepBoop(userInput) {
   let output = [];
   for (let i = 0; i <= userInput; i++) {
-    if (i.toString().includes(3)){
+    if (includesNumber(i, 3)){
       output.push("Won't you be my neighbor?");
-    } else if (i.toString().includes(2)){
+    } else if (includesNumber(i, 2)){
       output.push("Boop");
-    } else if (i.toString().includes(1)){
+    } else if (includesNumber(i, 1)){
       output.push("Beep");
     } else {
       output.push(i);
     }
   }
-
-
-
   return output
 }
